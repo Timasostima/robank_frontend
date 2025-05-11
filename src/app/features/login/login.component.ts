193734f0ebe0921b84fa89dcd48f775260138377
Tymur Kulivar <contact@tymurkulivar.dev>
2025-05-11 19:44:26 +0200

    implement login component

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject} from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
-import { Router, RouterLink } from "@angular/router"
+import { RouterLink } from "@angular/router"
 import {AuthService} from '../../core/services/auth.service';
 
 @Component({
@@ -12,4 +12,45 @@ import {AuthService} from '../../core/services/auth.service';
   styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
+  loginForm: FormGroup
+  submitted = false
+  showPassword = false
+  authService = inject(AuthService)
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  get f() {
+    return this.loginForm.controls
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword
+  }
+
+  onSubmit() {
+    this.submitted = true
+
+    if (this.loginForm.invalid) {
+      return
+    }
+
+    this.authService.logIn(this.f["email"].value, this.f["password"].value)
+      .then(user => {
+        console.log('Logged in:', user);
+      })
+      .catch(err => console.error("Login failed", err));
+  }
+
+  LoginGoogle() {
+    this.authService.signInWithGoogle()
+      .then(user => console.log('Google Log-in:', user))
+      .catch(err => console.error(err));
+  }
 }
