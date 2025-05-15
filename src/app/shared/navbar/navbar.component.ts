6516c6177a67cc5b2c9d298f9b070c34b777aaf5
Tymur Kulivar <contact@tymurkulivar.dev>
2025-05-15 12:00:47 +0200

    add navbar

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,79 @@
+import {Component, HostListener, inject, Input, OnInit} from "@angular/core"
+import {CommonModule} from "@angular/common"
+import {RouterLink, RouterLinkActive} from "@angular/router"
+import {AuthService} from '../../core/services/auth.service';
+
+@Component({
+  selector: "app-navbar",
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: "navbar.component.html",
+  styleUrls: ["navbar.component.css"],
+})
+export class NavbarComponent implements OnInit {
+  isMobile = false
+  menuOpen = false
+  authService = inject(AuthService)
+  isLoggedIn: boolean = false
+  profileMenuOpen = false
+
+  @HostListener("window:resize", ["$event"])
+  onResize(event: any) {
+    this.checkScreenSize()
+  }
+
+  @HostListener("document:click", ["$event"])
+  onDocumentClick(event: MouseEvent) {
+    // Close profile dropdown when clicking outside
+    if (this.profileMenuOpen) {
+      const target = event.target as HTMLElement
+      const profileButton = document.querySelector(".profile-button")
+      const profileDropdown = document.querySelector(".profile-dropdown")
+
+      if (profileButton && profileDropdown) {
+        if (!profileButton.contains(target) && !profileDropdown.contains(target)) {
+          this.profileMenuOpen = false
+        }
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.checkScreenSize()
+    this.authService.isLoggedIn$.subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768
+    if (!this.isMobile) {
+      this.menuOpen = false
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen
+  }
+
+  closeMenu() {
+    this.menuOpen = false
+  }
+
+  toggleProfileMenu() {
+    this.profileMenuOpen = !this.profileMenuOpen
+  }
+
+  logout() {
+    this.authService.logout()
+      .then(() => {
+        console.log("User logged out successfully");
+        window.location.href = '/login';
+      })
+      .catch(err => console.error("Logout failed", err));
+  }
+
+  toLandingPage() {
+    window.location.href = '/';
+  }
+}
